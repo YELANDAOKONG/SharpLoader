1a38e6b250efeaa6be0a60da3bb507aa47a2c61c
YELANDAOKONG <yelandaokong@yldk.xyz]>
2025-02-13 08:59:00 +0800

    Log4j2 Test

--- src/main/java/xyz/yldk/mcmod/SharpLoader/Main.java
+++ src/main/java/xyz/yldk/mcmod/SharpLoader/Main.java
@@ -1,10 +1,16 @@
 package xyz.yldk.mcmod.SharpLoader;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.lang.instrument.Instrumentation;
 
 public class Main {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Hello World!");
 
     }
 
