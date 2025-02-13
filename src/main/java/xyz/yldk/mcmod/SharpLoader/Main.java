3812669b3c0ed5b5889b1ff5fa2456d632473bf5
YELANDAOKONG <yelandaokong@yldk.xyz]>
2025-02-13 08:33:26 +0800

    INIT

--- src/main/java/xyz/yldk/mcmod/SharpLoader/Main.java
+++ src/main/java/xyz/yldk/mcmod/SharpLoader/Main.java
@@ -1,7 +1,19 @@
 package xyz.yldk.mcmod.SharpLoader;
 
+import java.lang.instrument.Instrumentation;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
+
+    public static void premain(String args, Instrumentation inst) {
+
+    }
+
+    public static void agentmain(String args, Instrumentation inst) {
+
+    }
+
 }
